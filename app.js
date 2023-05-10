8decb374ce127a953f6904243797676565ce9c29
Anurag Singh <anuragsinghchauhan456@gmail.com>
2023-05-10 16:45:50 +0530

    Todo List

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function(req, res){
+
+    let today = new Date();
+    
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+    
+    res.render("list", {kindOfDay: day, newListItems: items});
+
+});
+
+
+app.post("/", function(req, res){
+    let item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
+
+
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});
