0e27457cecf99357175ac42d506a5aeedb9c7e85
Anurag Singh <anuragsinghchauhan456@gmail.com>
2023-05-11 16:21:58 +0530

    Work List Added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -22,18 +23,34 @@ app.get("/", function(req, res){
 
     let day = today.toLocaleDateString("en-US", options);
     
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", {listTitle: day, newListItems: items});
 
 });
 
 
 app.post("/", function(req, res){
+
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
+
+});
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
 })
-
-
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
