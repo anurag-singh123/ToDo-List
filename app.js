d1c1489f0c14a3f69e51799b4e7cde1f042517ed
Anurag Singh <anuragsinghchauhan456@gmail.com>
2023-05-11 16:53:50 +0530

    created seprate file for date

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set("view engine", "ejs");
 
@@ -13,15 +14,7 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
-    let today = new Date();
-    
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
+    const day = date.getDate();
     
     res.render("list", {listTitle: day, newListItems: items});
 
@@ -30,7 +23,7 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
-    let item = req.body.newItem;
+    const item = req.body.newItem; 
 
     if(req.body.list === "Work"){
         workItems.push(item);
@@ -47,7 +40,7 @@ app.get("/work", function(req, res){
 });
 
 app.post("/work", function(req, res){
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     workItems.push(item);
     res.redirect("/work");
 })
