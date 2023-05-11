dc00a3614c7c2c61420580b598c20f2076df46bb
Anurag Singh <anuragsinghchauhan456@gmail.com>
2023-05-11 15:46:36 +0530

    CSS Updated

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let items = ["Buy Food", "Cook Food", "Eat Food"];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
